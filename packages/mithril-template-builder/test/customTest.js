1c84881576e457ea93d669cfe251c37124633afd
kevinkace <kevinacameron@hotmail.com>
2018-07-24 14:58:40 -0700

    Updated test

--- packages/mithril-template-builder/test/customTest.js
+++ packages/mithril-template-builder/test/customTest.js
@@ -1,7 +1,5 @@
 const templateBuilder = require("../dst/templateBuilder");
 
-console.log(typeof templateBuilder);
-
-const source = "<div>test</div>";
+const source = `<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 118"><title>health</title><polygon points="420.01 51.54 92.89 51.54 61.65 20.3 420.01 20.3 420.01 51.54"/><polygon points="420.01 75.43 115.86 75.43 98.41 57.98 420.01 57.98 420.01 75.43"/><circle cx="38.33" cy="38.33" r="23.33"/><circle cx="77.55" cy="79.67" r="23.33"/></svg>`;
 
 console.log(templateBuilder({ source }));
