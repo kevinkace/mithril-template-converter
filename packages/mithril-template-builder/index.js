da8df2ddc92b07aecef956fbea40ded41ef048a4
kevinkace <kevinacameron@hotmail.com>
2018-07-24 13:03:19 -0700

    New rollup script with npx, include built asset: ./dist/templateBuilder.js

--- packages/mithril-template-builder/index.js
+++ packages/mithril-template-builder/index.js
@@ -1,2 +1,2 @@
-import { templateBuilder } from "./src";
+import { templateBuilder } from "./src/index.js";
 export default templateBuilder;
---
+++ packages/mithril-template-builder/test/customTest.js
@@ -0,0 +1,3 @@
+const templateBuilder = require("../templateBuilder");
+
+console.log(typeof templateBuilder);
